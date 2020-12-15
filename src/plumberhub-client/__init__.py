254e1477fce68c149c84bb40b4e7f4c98281d414
Yi LIU <yi009@e.ntu.edu.sg>
2020-12-15 18:00:44 +0800

    [Refactor] lint code

--- src/plumberhub-client/__init__.py
+++ src/plumberhub-client/__init__.py
@@ -4,14 +4,16 @@ import asyncio
 import threading
 from src.plumberhub.plumberhub_pb2 import Sample
 
+
 def noop():
     pass
+
 
 class PlumberHubClient:
 
     def __init__(
-        self, hostname, port, clientId,
-        onsample = noop, onerror = noop, onclose = noop
+            self, hostname, port, clientId,
+            onsample=noop, onerror=noop, onclose=noop
     ):
         host = hostname + ':' + str(port)
         baseURL = 'http://' + host + '/api/sdk/client/' + clientId
@@ -29,21 +31,21 @@ class PlumberHubClient:
         def listen():
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            wsURL = 'ws://' + host + '/client/' + clientId +'/session?credential=' + credential
+            wsURL = 'ws://' + host + '/client/' + clientId + '/session?credential=' + credential
 
             async def sampleHandler():
-                async with websockets.connect(uri = wsURL) as ws:
+                async with websockets.connect(uri=wsURL) as ws:
                     while (True):
                         sample = Sample()
                         sample.MergeFromString(await ws.recv())
                         onsample(sample)
-        
+
             loop.run_until_complete(sampleHandler())
             onclose()
-        
-        listeningThread = threading.Thread(target = listen, args = ())
+
+        listeningThread = threading.Thread(target=listen, args=())
         listeningThread.start()
-    
+
     def __state(self):
         return self.__baseURL + '/device/state/'
 
@@ -60,7 +62,7 @@ class PlumberHubClient:
             return response.json()['value']
 
     def set(self, key, value):
-        response = requests.put(self.__state() + key, { 'value': value })
+        response = requests.put(self.__state() + key, {'value': value})
 
         if (response.status_code == 200):
             return response.json()['value']
